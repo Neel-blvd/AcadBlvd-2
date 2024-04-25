6023027c1c7ccb61fc1c45cb0bac0ad7224a55dc
Neel-blvd <neelphadke13579@gmail.com>
2024-04-25 20:40:52 +0530

    update about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,13 +10,6 @@ function About() {
         <p className='text-orange-600 text-lg font-mono pt-5'>
             It hosts previous year subjective and features some minimalistic quiz functionality.
         </p>
-        <br></br><br></br><br></br>
-        <p className='text-orange-600 text-3xl font-bold font-mono'>
-            Coming Soon!!!
-        </p>
-        <p className='text-orange-600 text-lg font-mono pt-5'>
-            The integration of a database to it to store user data such as performance, time spent etc.
-        </p>
     </div>
   )
 }
